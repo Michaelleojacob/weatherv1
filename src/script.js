5f078deb95258b4946a9ce3bf2011e608d8dcdeb
Michael <michaelleojacob@gmail.com>
2021-11-01 13:35:34 -0700

    added a feature that extracts useful properties from the weather data. still unsure how I want the toggle to communicate to render the tempatures in c or f

--- src/script.js
+++ src/script.js
@@ -1,25 +1,30 @@
 import './style.css';
 
+function extractUsefulProps(obj) {
+  return {
+    name: obj.name,
+    feelsLike: obj.main.feels_like,
+    temp: obj.main.temp,
+    max: obj.main.temp_max,
+    min: obj.main.temp_min,
+    desc: obj.weather[0].description,
+    icon: obj.weather[0].icon,
+    windspd: obj.wind.speed,
+  };
+}
+
+// this is the function to fetch a city
 async function fetchData(city) {
   const makeFetch = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.key}`,
     { mode: 'cors' }
   );
   const weatherData = await makeFetch.json();
-  console.log(weatherData);
+  const weatherProps = extractUsefulProps(weatherData);
+  console.log(weatherProps);
+  return weatherProps;
 }
 fetchData('san diego');
 
 const checkbox = document.querySelector('#checkbox');
 console.log(checkbox.checked);
-
-// we can call fetchData(city)
-// we can check the checkbox value
-
-// on submit
-// make fetch
-// depending on checkbox ->
-// -> render card -> f or c
-
-// Q how to have the card check for checkbox click
-// Q checkbox changes how to re render the card
