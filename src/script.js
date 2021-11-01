a8fa0224706dad8a976c82d34dda9bac03545ab8
Michael <michaelleojacob@gmail.com>
2021-10-31 22:31:42 -0700

    getting successful api fetch/request

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
-import './style.css';
-
-console.log('hi');
+async function fetchData(city) {
+  const makeFetch = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.key}`,
+    { mode: 'cors' }
+  );
+  const weatherData = await makeFetch.json();
+  console.log(weatherData);
+}
+fetchData('san diego');
